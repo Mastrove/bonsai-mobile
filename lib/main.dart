fe9ebadefb218d265efe2ae5c270e9b8310332bf
eben-k <ennim.consults@gmail.com>
2020-04-07 15:46:14 +0100

    build home screen build out individual components of the home screen install relevant packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
-import 'package:accpatapp/components/app_scaffold.dart';
 import 'package:accpatapp/models/comment_data.dart';
 import 'package:accpatapp/models/comment_reply_data.dart';
-import 'package:accpatapp/models/feed_data.dart';
-import 'package:accpatapp/models/notification_data.dart';
-import 'package:accpatapp/models/partner_data.dart';
 import 'package:accpatapp/models/sprint_data.dart';
 import 'package:accpatapp/screens/home_screen.dart';
 import 'package:flutter/material.dart';
@@ -19,12 +15,9 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (context) => SprintData()),
         ChangeNotifierProvider(create: (context) => CommentData()),
         ChangeNotifierProvider(create: (context) => CommentReplyData()),
-        ChangeNotifierProvider(create: (context) => NotificationData()),
-        ChangeNotifierProvider(create: (context) => FeedData()),
-        ChangeNotifierProvider(create: (context) => PartnerData()),
       ],
       child: MaterialApp(
-        home: AppScaffold(),
+        initialRoute: HomeScreen.id,
         routes: {
           HomeScreen.id: (context) => HomeScreen(),
         },
