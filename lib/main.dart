ca709e83092a930598399cf97b72de9de5ec4665
eben-k <ennim.consults@gmail.com>
2020-04-09 14:07:19 +0100

    build feeds screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:accpatapp/components/app_scaffold.dart';
 import 'package:accpatapp/models/comment_data.dart';
 import 'package:accpatapp/models/comment_reply_data.dart';
+import 'package:accpatapp/models/feed_data.dart';
+import 'package:accpatapp/models/notification_data.dart';
 import 'package:accpatapp/models/sprint_data.dart';
 import 'package:accpatapp/screens/home_screen.dart';
 import 'package:flutter/material.dart';
@@ -15,9 +18,11 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (context) => SprintData()),
         ChangeNotifierProvider(create: (context) => CommentData()),
         ChangeNotifierProvider(create: (context) => CommentReplyData()),
+        ChangeNotifierProvider(create: (context) => NotificationData()),
+        ChangeNotifierProvider(create: (context) => FeedData()),
       ],
       child: MaterialApp(
-        initialRoute: HomeScreen.id,
+        home: AppScaffold(),
         routes: {
           HomeScreen.id: (context) => HomeScreen(),
         },
