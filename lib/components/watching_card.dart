03154e0181a1fc9d719f37798726498f0d445efc
eben-k <ennim.consults@gmail.com>
2020-04-09 19:16:29 +0100

    build watching screen

--- lib/components/watching_card.dart
+++ lib/components/watching_card.dart
@@ -1,26 +1,31 @@
-import 'package:accpatapp/components/notification_row_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class FeedCard extends StatelessWidget {
-  final String feedDate;
-  final String feedDetails;
+class WatchingCard extends StatelessWidget {
+  final String watchingName;
+  final String watchingDetails;
+  final String watchingDuration;
+  final Color avatarColor;
 
-  FeedCard({this.feedDetails, this.feedDate});
+  WatchingCard(
+      {this.avatarColor,
+      this.watchingName,
+      this.watchingDuration,
+      this.watchingDetails});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         border: Border(
-          top: BorderSide(
+          bottom: BorderSide(
             width: 0.5,
             color: Color(0xFFC4C4C4),
           ),
         ),
       ),
       padding: EdgeInsets.only(
-        top: 10.0,
+        top: 30.0,
         bottom: 20.0,
       ),
       child: Row(
@@ -34,7 +39,7 @@ class FeedCard extends StatelessWidget {
               borderRadius: BorderRadius.circular(28.0),
             ),
             child: CircleAvatar(
-              backgroundColor: Colors.white,
+              backgroundColor: avatarColor,
               radius: 28.0,
             ),
           ),
@@ -47,12 +52,12 @@ class FeedCard extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 Text(
-                  feedDate,
+                  watchingName,
                   style: GoogleFonts.roboto(
                     fontStyle: FontStyle.normal,
-                    fontWeight: FontWeight.w300,
+                    fontWeight: FontWeight.w500,
                     fontSize: 12.0,
-                    color: Color(0xFF6C5E5E),
+                    color: Color(0xFF000000),
                   ),
                   softWrap: true,
                 ),
@@ -62,24 +67,25 @@ class FeedCard extends StatelessWidget {
                 Flexible(
                   fit: FlexFit.loose,
                   child: Text(
-                    feedDetails,
+                    watchingDetails,
                     style: GoogleFonts.roboto(
                       fontStyle: FontStyle.normal,
                       fontWeight: FontWeight.w300,
                       fontSize: 14.0,
-                      color: Color(0xFF6C5E5E),
+                      color: Color(0xFF000000),
                     ),
                   ),
                 ),
-                Container(
-                  padding: EdgeInsets.only(
-                    top: 12.0,
-                  ),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      NotificationRowIcons(),
-                    ],
+                SizedBox(
+                  height: 5.0,
+                ),
+                Text(
+                  watchingDuration,
+                  style: GoogleFonts.roboto(
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.w300,
+                    fontSize: 12.0,
+                    color: Color(0xFF6C5E5E),
                   ),
                 ),
               ],
--- lib/screens/watching_screen.dart
+++ lib/screens/watching_screen.dart
@@ -1,8 +1,37 @@
+import 'package:accpatapp/components/watching_list.dart';
 import 'package:flutter/material.dart';
+import 'package:accpatapp/components/app_bar_title.dart';
 
 class WatchingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: EdgeInsets.only(
+            top: 8.0,
+            left: 12.0,
+            bottom: 8.0,
+          ),
+          child: CircleAvatar(
+            backgroundColor: Color(0xFFFF740F),
+            radius: 22.0,
+          ),
+        ),
+        title: AppBarTitle(
+          titleText: 'Watching',
+        ),
+        backgroundColor: Color(0xFF1480ff),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.only(
+            top: 24.0,
+            left: 12.0,
+          ),
+          child: WatchingList(),
+        ),
+      ),
+    );
   }
 }
